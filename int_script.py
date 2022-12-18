f8d463377623c71f0375b661348fdf39adc23efc
Mateusz Krasoń <s180450@student.pg.edu.pl>
2022-12-18 01:00:43 +0100

    should work

--- int_script.py
+++ int_script.py
@@ -1,38 +1,59 @@
 import scapy 
 from scapy.all import *
 
-class UDPshim(Packet) :
-    fields_desc = [
-            BitField("Type", 0, 4), 
-            BitField("R1", 0, 1),
-            BitField("R2",0, 1),
-            BitField("NPT", 0, 2),
-            BitField("Length", 0, 8),
-            BitField("UDP_PORT", 0, 16)
-            ]
+interfaces = ["veth-output", "enp0s3"]
+
 class main_headers(Packet):
     fields_desc = []
+class UDPshim(Packet) :
+    fields_desc = [
+            BitField("Type", 1, 1), 
+            BitField("NPT", 1, 2),
+            BitField("R1", 0, 1),
+            BitField("R2",0, 1),
+            BitField("Length", 7, 8),
+            BitField("UDP_PORT", NWM, 16)
+            ]
+
 class UDP_INT_HEADER(Packet) :
-    fields_desc = []
+    fields_desc = [
+            BitField("Version", 2, 4),
+            BitField("D", 0, 1),
+            BitField("E", 0, 1),
+            BitField("M", 0, 1),
+            BitField("R", 0, 12),
+            BitField("HopML", 2, 5),
+            BitField("RemainingHopC", 6, 8),
+            BitField("InstructBitmap", 36864, 16),
+            BitField("DomSpecificID", 0, 16),
+            BitField("DSInstr", 0, 16),
+            BitField("DSFlags", 0, 16),
+
+
+    ]
 class INT_METADANE(Packet):
     fields_desc = []
-if __name__ == "__main__" :
-    #pkt = bytes([0 for i in range(1000)])
-    #x = main_headers(pkt[0:do_konca_headera1])
-    #udp_payload = pkt[do_konca_header1:]
-    #shim = UDPshim()
+
+def forward_and_change(pkt) :
+    pkt = bytes([0 for i in range(64)])
+    x = main_headers(pkt[0:41])
+    udp_payload = pkt[41:]
+    shim = UDPshim()
     #przypisz tu dane do shima
-    #header = UDP_INT_HEADER()
+    header = UDP_INT_HEADER()
     #znowu przypisz tu dane
-    #metadane = INT_METADANE()
+    metadane = INT_METADANE()
     #znowu przypisz dane
     packet = Packet()
     z = UDPshim()
-    #packet /=z
-    #packet /=z
-    packet.show()
     packet /=x
-
+    packet /=shim
+    packet /=header
+    packet /=metadane
+    packet /=udp_payload
+    print(pkt.show())
+    sendp(pkt, iface=interfaces[1])
+sniff(iface=interfaces[0], prn=forward_and_change)
 
 
 
